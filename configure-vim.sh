5d90e65bfcb1db6a14b4633509b9c4fa813b53cd
Alexander Szmidt <alexander.szmidt@gmail.com>
2017-10-27 19:02:16 +0200

    Add configure-vim script

---
+++ configure-vim.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+BACKUP=./backup
+ASSETS=./assets
+CP="cp -iv"
+RM="rm -rfv"
+LN="ln -svi"
+function realpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
+
+if (( $EUID == 0 )); then
+	echo "This script must be NOT run as root." 1>&2
+	exit 1
+fi
+
+cd $(dirname $0)
+
+$CP ~/.vim ~/.vimrc $BACKUP/
+$RM ~/.vim ~/.vimrc
+sudo apt purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox &&
+$LN $(realpath "$ASSETS/vim") ~/.vim
+sudo apt build-dep vim &&
+sudo apt install python-dev python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git build-essential cmake
+cd ./vim
+make distclean &&
+./configure --with-features=huge \
+            --enable-multibyte \
+            --disable-gui \
+            --enable-fail-if-missing \
+            --enable-luainterp \
+            --enable-perlinterp \
+            --enable-pythoninterp \
+            --enable-python3interp \
+            --enable-rubyinterp \
+            --enable-cscope \
+            --enable-terminal &&
+make &&
+sudo make install &&
+cd -
+vim +PlugInstall +qall
+
+cd $(realpath $ASSETS/vim/plugged/YouCompleteMe); ./install.py --all; cd -
+
+sudo update-alternatives --install /usr/bin/editor editor $(which vim) 1
+sudo update-alternatives --set editor $(which vim)
+sudo update-alternatives --install /usr/bin/vi vi $(which vim) 1
+sudo update-alternatives --set vi /usr/bin/vim

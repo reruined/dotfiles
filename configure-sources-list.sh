72cf6ab7c58a028f46d60fea97e648c7cce273bd
Alexander Szmidt <alexander.szmidt@gmail.com>
2017-10-27 20:16:27 +0200

    Improvements

--- configure-sources-list.sh
+++ configure-sources-list.sh
@@ -11,6 +11,10 @@ fi
 
 cd $(dirname $0)
 
+$CP /etc/apt/sources.list $BACKUP &&\
+$CP $ASSETS/sources.list /etc/apt/sources.list &&\
+$CP $ASSETS/pin /etc/apt/preferences.d/pin &&
+
 apt install curl wget apt-transport-https dirmngr &&\
 curl https://www.franzoni.eu/keys/D401AB61.txt | apt-key add - &&\
 wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && dpkg -i deb-multimedia-keyring_2016.8.1_all.deb &&\
@@ -21,6 +25,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - &&\
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&\
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1 &&\
-$CP /etc/apt/sources.list $BACKUP &&\
-$CP $ASSETS/sources.list /etc/apt/sources.list &&\
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian stretch main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 apt update
+
--- configure-vim.sh
+++ configure-vim.sh
@@ -14,33 +14,33 @@ fi
 
 cd $(dirname $0)
 
-$CP ~/.vim ~/.vimrc $BACKUP/
-$RM ~/.vim ~/.vimrc
-sudo apt purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox &&
-$LN $(realpath "$ASSETS/vim") ~/.vim
-sudo apt build-dep vim &&
-sudo apt install python-dev python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git build-essential cmake
-cd ./vim
-make distclean &&
-./configure --with-features=huge \
-            --enable-multibyte \
-            --disable-gui \
-            --enable-fail-if-missing \
-            --enable-luainterp \
-            --enable-perlinterp \
-            --enable-pythoninterp \
-            --enable-python3interp \
-            --enable-rubyinterp \
-            --enable-cscope \
-            --enable-terminal &&
-make &&
-sudo make install &&
-cd -
-vim +PlugInstall +qall
+#$CP ~/.vim ~/.vimrc $BACKUP/
+#$RM ~/.vim ~/.vimrc
+#sudo apt purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox &&
+#$LN $(realpath "$ASSETS/vim") ~/.vim
+#sudo apt build-dep vim &&
+sudo apt install python-dev python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git build-essential cmake mono-devel golang rustc cargo
+#cd ./vim
+#make distclean &&
+#./configure --with-features=huge \
+#            --enable-multibyte \
+#            --disable-gui \
+#            --enable-fail-if-missing \
+#            --enable-luainterp \
+#            --enable-perlinterp \
+#            --enable-pythoninterp \
+#            --enable-python3interp \
+#            --enable-rubyinterp \
+#            --enable-cscope \
+#            --enable-terminal &&
+#make &&
+#sudo make install &&
+#cd -
+#vim +PlugInstall +qall
 
 cd $(realpath $ASSETS/vim/plugged/YouCompleteMe); ./install.py --all; cd -
 
-sudo update-alternatives --install /usr/bin/editor editor $(which vim) 1
-sudo update-alternatives --set editor $(which vim)
-sudo update-alternatives --install /usr/bin/vi vi $(which vim) 1
-sudo update-alternatives --set vi /usr/bin/vim
+#sudo update-alternatives --install /usr/bin/editor editor $(which vim) 1
+#sudo update-alternatives --set editor $(which vim)
+#sudo update-alternatives --install /usr/bin/vi vi $(which vim) 1
+#sudo update-alternatives --set vi /usr/bin/vim

8918f05a24c4b5fa593b000f6996b5d2704ffcce
Alexander Szmidt <alexander.szmidt@gmail.com>
2018-06-08 11:36:33 +0200

    Updates

--- assets/awesome/rc.lua
+++ assets/awesome/rc.lua
@@ -58,7 +58,6 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    --awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -74,6 +73,7 @@ awful.layout.layouts = {
     --awful.layout.suit.corner.ne,
     --awful.layout.suit.corner.sw,
     --awful.layout.suit.corner.se,
+    awful.layout.suit.floating
 }
 -- }}}
 
